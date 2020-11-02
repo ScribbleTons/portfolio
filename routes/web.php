4e921287cf098a8da8f6c32968d709d429d1dc7a
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2020-11-02 02:49:17 -0800

    Optimized image

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::post('/contact', [ContactController::class, 'store'])->name('contact.stor
 Route::get('/download/{file}', [DownloadsController::class, 'download'])->name('download');
 
 
-Route::middleware(['auth:sanctum', 'verified'])->group( function () {
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/dashboard',[HomeController::class, 'index']);
     Route::get('/contact/list', [ContactController::class, 'index'])->name('contact.list');
     Route::get('/contact/{contact}', [ContactController::class, 'show'])->name('contact.view');
